58cad4446656a80bbd0ca7d40873d283cfdd22d1
Ragzgit <44136308+Ragzgit@users.noreply.github.com>
2018-10-14 18:16:42 +0530

    Create DisplayDate.js

---
+++ DisplayDate.js
@@ -0,0 +1,11 @@
+var date = new Date();
+var day = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+
+var hours = date.getHours();
+var min = date.getMinutes();
+
+if (min < 10) min = '0' + min;
+
+document.getElementById('date_time').innerHTML = "Today\'s Date is " + day + "/" + month + "/" + year + "/";
