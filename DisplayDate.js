a9241ade2307527e35ae3ad68490f072ca4d69c4
Ragzgit <44136308+Ragzgit@users.noreply.github.com>
2018-10-14 19:06:52 +0530

    Update DisplayDate.js

--- DisplayDate.js
+++ DisplayDate.js
@@ -5,4 +5,5 @@ var year = date.getFullYear();
 var hours = date.getHours();
 var min = date.getMinutes();
 if (min < 10) min = '0' + min;
-document.getElementById("dat").innerHTML = "Today\'s Date is " + day + "/" + month + "/" + year ;;
+document.getElementById("dat").innerHTML = "Today\'s Date: " + day + "/" + month + "/" + year ;
+document.getElementById("time").innerHTML = "Time: " + hours + ":" + min;
