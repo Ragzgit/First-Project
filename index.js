bba8b991dba8cba614501e1f9b07525a2a2b929a
Ragzgit <44136308+Ragzgit@users.noreply.github.com>
2018-10-22 12:17:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('./index.html', null, function(err, data) { 
+    if(err) {
+      res.writeHead(404);
+      res.write('file ah kaanomeh...')
+    }
+    else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    }
+    
+    res.end();
+  });
+}).listen(8080);
